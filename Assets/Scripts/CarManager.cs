3f6eb733d8bca5cddf71d8fb5de8ac0a86d634d7
Stavrin <steffanleprince@gmail.com>
2022-10-09 20:27:57 +0100

    1st script "CarManager" written. 2d car assets added.

---
+++ Assets/Scripts/CarManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarManager : MonoBehaviour { //everything public because this will all be used outside of the class definition.
+
+    public List<CarBase> cars = new List<CarBase>(); //the list of all the different types of car and their characteristics.
+                                                        //will include string name, int price, colour
+
+    [System.Serializable] //so that I can use Unity editor to set these.
+    public class CarBase
+    {
+        public Text CarName; //text as oppose to string so that I can use it with Unity UI.
+        public GameObject CarPrefab; //will drag and drop the prefab of the car here.
+        public AudioClip CarHorn; //audio clip to pay when this car is bought.
+        public int price; //how much the car costs in £, the total price will be this plus any modifications to the car.
+
+
+
+        public enum CarType //dropdown box of choices, could be usined with animator, might take this out.
+        {
+            offroad,
+            racing,
+            truck
+        }
+
+    }
+
+}
