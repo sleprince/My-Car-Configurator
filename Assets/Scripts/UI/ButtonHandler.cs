ea6ad4ca320757134718b1c2a64af28c2c24b707
Stavrin <steffanleprince@gmail.com>
2022-10-12 21:15:27 +0100

    Car name appears in UI too, tidying ButtonHandler script.

--- Assets/Scripts/UI/ButtonHandler.cs
+++ Assets/Scripts/UI/ButtonHandler.cs
@@ -8,16 +8,16 @@ public class ButtonHandler : MonoBehaviour
 {
 
     public Transform CarSpawnPoint; //an empty GameObject with the transform position for the car.
-    //public int PreviousClicks = 0;
-    public int NextClicks;
-    public GameObject Car;
+    public int NextClicks; //amount of times the button has been clicked.
     public Button PreviousButton; //the buttons themselves.
     public Button NextButton;
+    public Text CarName;
 
-    CarManager CarMan;
+    CarManager CarMan; //reference to my CarManager.
 
-    //note for future reference, awake and start never get called on buttons.
+    //note for future reference, awake and start never get called if you use clickon() buttons in editor.
     // void awake()
+
     void start()
     {
 
@@ -27,28 +27,20 @@ public class ButtonHandler : MonoBehaviour
     //this works instead for a disabled object.
     public ButtonHandler() //equivalent to wake.
     {
-        instance = this;
-
-        //PreviousButton.gameObject.SetActive(false);
-
-
+        instance = this; //part of the method that lets me use methods from this class in other scripts.
 
     }
 
-     void OnEnable() //like start
+     void OnEnable() //like start.
     {
-        PreviousButton.onClick.AddListener(Previous);
+        PreviousButton.onClick.AddListener(Previous); //if clicked run method Previous.
         NextButton.onClick.AddListener(Next);
     }
 
     public void Update()
     {
-
-
-
-
-
-
+        CarName.text = CarMan.Chosen[0].CarName.text ; //make the car name appear in the UI.
+        Debug.Log(CarName.text);
 
     }
 
@@ -56,37 +48,19 @@ public class ButtonHandler : MonoBehaviour
     //on click of the next button.
     public void Next()
     {
-        CarMan = CarManager.GetInstance();
 
+        CarBuilder();
 
-        // GameObject GO = Instantiate(Car) as GameObject;
-
-        if (CarMan.Chosen[0].ClonedCar != null)//If there's already a car there, destroy it before making the new one.
-        {
-            Destroy(CarMan.Chosen[0].ClonedCar);
-        }
-
-        GameObject GO = Instantiate(
-        //creates a car from the prefab
-        CarManager.GetInstance().ReturnCarWithID(NextClicks).CarPrefab[0],
-        //makes it appear at the position that I want.
-        CarSpawnPoint.position,
-        Quaternion.identity) as GameObject;
-
-        CarMan.Chosen[0].ClonedCar = GO;
-
-        if (NextClicks < 5)
+        if (NextClicks < 5)         //to make a different car appear each time the button is clicked.
         {
             NextClicks++;
         }
-        //to make a different car appear each time the button is clicked.
 
-
-        if (NextClicks == 5)
+        if (NextClicks == 5) //to stop NextClicks being a higher number than that in the list of cars.
         {
             NextClicks--;
             Previous(); //to stop you having to click twice before anything happens.
-            NextButton.gameObject.SetActive(false);
+            NextButton.gameObject.SetActive(false); //to stop user being able to go past the number of cars.
         }
 
         else
@@ -96,36 +70,16 @@ public class ButtonHandler : MonoBehaviour
 
         if (NextClicks != 0)
         {
-            PreviousButton.gameObject.SetActive(true);
+            PreviousButton.gameObject.SetActive(true); //make the other button visible whenever relevant.
         }
 
     }
 
     //on click of the previous button.
-    public void Previous() //pass in as clicks
+    public void Previous() 
     {
 
-        CarMan = CarManager.GetInstance();
-
-        //Car = Resources.Load("4x4_blue") as GameObject;
-
-        // GameObject GO = Instantiate(Car) as GameObject;
-
-
-            if (CarMan.Chosen[0].ClonedCar != null)//If there's already a car there, destroy it before making the new one.
-            {
-            Destroy(CarMan.Chosen[0].ClonedCar);
-            }
-
-
-            GameObject GO = Instantiate(
-            //creates a car from the prefab
-            CarManager.GetInstance().ReturnCarWithID(NextClicks).CarPrefab[0],
-            //makes it appear at the position that I want.
-            CarSpawnPoint.position,
-            Quaternion.identity) as GameObject;
-
-            CarMan.Chosen[0].ClonedCar = GO;
+        CarBuilder();
 
         //to make a different car appear each time the button is clicked.
         if (NextClicks > -1)
@@ -156,6 +110,30 @@ public class ButtonHandler : MonoBehaviour
 
 
     }
+
+    public void CarBuilder()
+    {
+        CarMan = CarManager.GetInstance();
+
+
+        if (CarMan.Chosen[0].ClonedCar != null)//If there's already a car there, destroy it before making the new one.
+        {
+            Destroy(CarMan.Chosen[0].ClonedCar);
+        }
+
+
+        GameObject GO = Instantiate(
+        //creates a car from the prefab
+        CarManager.GetInstance().ReturnCarWithID(NextClicks).CarPrefab[0],
+        //makes it appear at the position that I want.
+        CarSpawnPoint.position,
+        Quaternion.identity) as GameObject;
+
+        CarMan.Chosen[0].ClonedCar = GO; //this was for debugging purposes.
+        CarMan.Chosen[0].CarName = CarManager.GetInstance().ReturnCarWithID(NextClicks).CarName; //put the relevant CarName in
+                                                                                                 //the Chosen list.
+    }
+
 
     public static ButtonHandler instance;
     public static ButtonHandler GetInstance() //all of this is so that I can call this script from objects that do not have it attached.
